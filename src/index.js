8d03fa7dd6d2fce80cf7262bb7eb2f78d6d8a76c
Ankit <ankitjangir232323@gmail.com>
2023-01-04 18:34:04 +0530

    version1

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const path = require("path");
+const hbs = require("hbs");
+const temp_path = path.join(__dirname, "../views");
+console.log(temp_path);
+const partials_path = path.join(__dirname, "../views/partials");
+app.set("view engine", "hbs");
+app.set("views", temp_path);
+app.use("/public", express.static(path.join(__dirname, "../assets")));
+app.use(
+    "/css",
+    express.static(path.join(__dirname, "../node_modules/bootstrap/dist/css"))
+);
+app.use(
+    "/js",
+    express.static(path.join(__dirname, "../node_modules/bootstrap/dist/js"))
+);
+app.use(
+    "/js",
+    express.static(path.join(__dirname, "../node_modules/bootstrap/dist/js"))
+);
+
+
+app.use(express.urlencoded({ extended: false }));
+
+hbs.registerPartials(partials_path);
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.listen(port, () => {
+    console.log(`listing to the port at ${port}`);
+});
