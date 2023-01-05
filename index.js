345c1378567729855ac831b9d724caa01c164b61
Ankit <ankitjangir232323@gmail.com>
2023-01-05 10:17:42 +0530

    first commit

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const app = express();
 const port = process.env.PORT || 3000;
 const path = require("path");
 const hbs = require("hbs");
-const temp_path = path.join(__dirname, "../views");
+const temp_path = path.join(__dirname, "/views");
 console.log(temp_path);
-const partials_path = path.join(__dirname, "../views/partials");
+const partials_path = path.join(__dirname, "/views/partials");
 app.set("view engine", "hbs");
 app.set("views", temp_path);
-app.use("/public", express.static(path.join(__dirname, "../assets")));
+app.use("/public", express.static(path.join(__dirname, "/assets")));
 app.use(
     "/css",
-    express.static(path.join(__dirname, "../node_modules/bootstrap/dist/css"))
+    express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css"))
 );
 app.use(
     "/js",
-    express.static(path.join(__dirname, "../node_modules/bootstrap/dist/js"))
+    express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js"))
 );
 app.use(
     "/js",
-    express.static(path.join(__dirname, "../node_modules/bootstrap/dist/js"))
+    express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js"))
 );
 
 
